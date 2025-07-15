982567e2d065844fc2a2de6d77f640cfacd7c279
Ahmet Buğra Kurnaz <ahmetbugrakurnaz0@gmail.com>
2025-07-16 00:10:08 +0300

    register page

--- my-medusa-store-storefront/src/modules/account/components/register/index.tsx
+++ my-medusa-store-storefront/src/modules/account/components/register/index.tsx
@@ -21,30 +21,30 @@ const Register = ({ setCurrentView }: Props) => {
       data-testid="register-page"
     >
       <h1 className="text-large-semi uppercase mb-6">
-        Become a Medusa Store Member
+        Moda Es Es Üyesi Ol
       </h1>
       <p className="text-center text-base-regular text-ui-fg-base mb-4">
-        Create your Medusa Store Member profile, and get access to an enhanced
-        shopping experience.
+        Moda Es Es üye profilinizi oluşturun ve gelişmiş alışveriş deneyimine
+        erişim kazanın.
       </p>
       <form className="w-full flex flex-col" action={formAction}>
         <div className="flex flex-col w-full gap-y-2">
           <Input
-            label="First name"
+            label="Ad"
             name="first_name"
             required
             autoComplete="given-name"
             data-testid="first-name-input"
           />
           <Input
-            label="Last name"
+            label="Soyad"
             name="last_name"
             required
             autoComplete="family-name"
             data-testid="last-name-input"
           />
           <Input
-            label="Email"
+            label="E-posta"
             name="email"
             required
             type="email"
@@ -52,14 +52,14 @@ const Register = ({ setCurrentView }: Props) => {
             data-testid="email-input"
           />
           <Input
-            label="Phone"
+            label="Telefon"
             name="phone"
             type="tel"
             autoComplete="tel"
             data-testid="phone-input"
           />
           <Input
-            label="Password"
+            label="Şifre"
             name="password"
             required
             type="password"
@@ -69,33 +69,33 @@ const Register = ({ setCurrentView }: Props) => {
         </div>
         <ErrorMessage error={message} data-testid="register-error" />
         <span className="text-center text-ui-fg-base text-small-regular mt-6">
-          By creating an account, you agree to Medusa Store&apos;s{" "}
+          Hesap oluşturarak Moda Es Es&apos;in{" "}
           <LocalizedClientLink
             href="/content/privacy-policy"
             className="underline"
           >
-            Privacy Policy
+            Gizlilik Politikası
           </LocalizedClientLink>{" "}
-          and{" "}
+          ve{" "}
           <LocalizedClientLink
             href="/content/terms-of-use"
             className="underline"
           >
-            Terms of Use
+            Kullanım Şartları
           </LocalizedClientLink>
-          .
+          &apos;nı kabul etmiş olursunuz.
         </span>
         <SubmitButton className="w-full mt-6" data-testid="register-button">
-          Join
+          Üye Ol
         </SubmitButton>
       </form>
       <span className="text-center text-ui-fg-base text-small-regular mt-6">
-        Already a member?{" "}
+        Zaten üye misiniz?{" "}
         <button
           onClick={() => setCurrentView(LOGIN_VIEW.SIGN_IN)}
           className="underline"
         >
-          Sign in
+          Giriş yap
         </button>
         .
       </span>
